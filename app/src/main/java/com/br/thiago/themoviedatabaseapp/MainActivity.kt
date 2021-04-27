798155f9523937b068af98f4137f1b3dc8d2bcf4
Thiago Moreira de Souza <thiago.moreira.souza@gmail.com>
2021-04-27 08:53:10 -0300

    Navigation with Bottom Navigation

--- app/src/main/java/com/br/thiago/themoviedatabaseapp/MainActivity.kt
+++ app/src/main/java/com/br/thiago/themoviedatabaseapp/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.br.thiago.themoviedatabaseapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.br.thiago.themoviedatabaseapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setupBottomNavigation()
     }
+
+    private fun setupBottomNavigation() {
+        val navController = findNavController(R.id.fragment)
+        binding.bottomNavigationView.setupWithNavController(navController)
+    }
+
 }
